cdebf7f9627330c545425d48af44209079848df2
Sriragavendhiran <ragavenragaven06@gmail.com>
2025-03-29 15:34:08 +0530

    change

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -20,7 +20,7 @@ const Signup = () => {
     }
 
     try {
-      const response = await fetch("http://localhost:5000/api/auth/signup", {
+      const response = await fetch("https://auth-wd04.onrender.com/api/auth/signup", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
